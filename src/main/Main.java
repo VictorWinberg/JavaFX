cbae06770088bf20a9c67b64015b2c24475d3017
VictorWinberg <dat14vwi@student.lu.se>
2016-03-18 18:29:06 +0100

    Alert box

--- src/main/Main.java
+++ src/main/Main.java
@@ -31,8 +31,8 @@ public class Main extends Application {
 		scene1 = new Scene(layout1, 200, 200);
 		
 		//Button 2
-		Button button2 = new Button("Go BACK to scene 1");
-		button2.setOnAction(e -> window.setScene(scene1));
+		Button button2 = new Button("Click me!");
+		button2.setOnAction(e -> AlertBox.display("AlertTitle", "This is a cool alert box"));
 		
 		//Layout 2
 		StackPane layout2 = new StackPane();
