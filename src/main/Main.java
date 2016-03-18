24c907b96dbdb3b00c8640a2ee52f6d3ff457cae
VictorWinberg <dat14vwi@student.lu.se>
2016-03-18 19:44:33 +0100

    CSS added

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,13 @@
 package main;
 
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -26,9 +24,7 @@ public class Main extends Application {
 		window.setTitle("victorwinberg - JavaFX");
 		
 		Scene sceneLogin = getLoginScene();
-		
 		Scene mainScene = getMainScene(sceneLogin);
-
 		Scene sceneWelcome = getWelcomeScene(mainScene);
 		
 		window.setScene(sceneWelcome);
@@ -90,8 +86,9 @@ public class Main extends Application {
 		GridPane.setConstraints(loginButton, 1, 2);
 		
 		grid.getChildren().addAll(nameLabel, nameField, passLabel, passField, loginButton);
-		
-		return new Scene(grid, 300, 200);
+		Scene scene = new Scene(grid, 300, 200);
+		scene.getStylesheets().add(getClass().getResource("/styles/ViperTheme.css").toExternalForm());
+		return scene;
 	}
 	
 }
